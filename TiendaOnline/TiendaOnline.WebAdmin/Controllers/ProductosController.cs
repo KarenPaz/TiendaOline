ea615c0e72b55c46abd9a1f16d595543f0f7701b
Karen Paz <mabelpazf@gmail.com>
2021-06-23 00:07:20 -0600

    Agregando mantenimiento Productos

---
+++ TiendaOnline/TiendaOnline.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaOnline.BL;
+
+namespace TiendaOnline.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosBL _productosBL;
+
+        public ProductosController()
+        {
+            _productosBL = new ProductosBL();
+
+        }
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var ListadeProductos = _productosBL.ObtenerProductos();
+
+            return View(ListadeProductos);
+        }
+
+
+        public ActionResult Crear()
+        {
+            var nuevoProducto = new Producto();
+
+            return View(nuevoProducto);
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Producto producto)
+        {
+            _productosBL.GuardarProducto(producto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
